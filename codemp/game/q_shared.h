cb1acb4f8ce074520118229ffe3ee2a683fc1372
Atlas <julien@.(none)>
2011-05-16 23:58:17 +0200

    Revert "Correctif de sÃ©curitÃ© concernant un problÃ¨me de stabilitÃ© en liaison avec les votes."
    This reverts commit 286ad031dd417191074fca649db7c8f0689f64a3.

--- codemp/game/q_shared.h
+++ codemp/game/q_shared.h
@@ -376,7 +376,7 @@ typedef int		clipHandle_t;
 // the game guarantees that no string from the network will ever
 // exceed MAX_STRING_CHARS
 #define	MAX_STRING_CHARS	1024	// max length of a string passed to Cmd_TokenizeString
-#define	MAX_STRING_TOKENS	75	// max tokens resulting from Cmd_TokenizeString
+#define	MAX_STRING_TOKENS	1024	// max tokens resulting from Cmd_TokenizeString
 #define	MAX_TOKEN_CHARS		1024	// max length of an individual token
 
 #define	MAX_INFO_STRING		1024
